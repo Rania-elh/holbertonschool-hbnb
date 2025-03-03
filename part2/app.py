4892eda0e3be9774870bad50903d62c611742df8
Rania-elh <rania1elhefnawy@gmail.com>
2025-03-03 01:33:17 +0100

    HBNB Part2

--- part2/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restx import Api
-
-app = Flask(__name__)
-api = Api(app, version="1.0", title="HBnB API", description="API pour HBnB Evolution")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
